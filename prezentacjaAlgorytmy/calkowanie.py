449b0080d2c56a064e864f99219126e55f5d8b96
hcnt <jacek.kusmierczyk@wp.pl>
2019-03-20 22:07:03 +0100

    bisekcja i całkowanie

--- prezentacjaAlgorytmy/calkowanie.py
+++ prezentacjaAlgorytmy/calkowanie.py
@@ -1,4 +1,6 @@
 import random as rand
+import matplotlib.pyplot as plt
+import matplotlib.axes as axes
 
 
 def prostokaty(f, a, b, n):
@@ -32,9 +34,40 @@ def monteCarlo(f, a, b, n):
 
 
 def funkcja(x):
-    return x ** 2
+    return (6 * x ** 3) + (3 * x ** 2) - (16 * x) + 12
 
 
-print(prostokaty(funkcja, 0, 4, 10))
-print(trapezy(funkcja, 0, 4, 10))
-print(monteCarlo(funkcja, 0, 4, 10000))
+def wykres():
+    dolnaGranica = 1
+    gornaGranica = 200
+    wynik = prostokaty(funkcja, dolnaGranica, gornaGranica, 5000)
+    wynikiProstokaty = []
+    wynikiTrapezy = []
+    wynikiMonteCarlo = []
+    a,b = 1, 11
+    for i in range(a, b):
+        # print(i)
+        wynikiProstokaty.append(prostokaty(funkcja, dolnaGranica, gornaGranica, i))
+        wynikiTrapezy.append(trapezy(funkcja, dolnaGranica, gornaGranica, i))
+        # wynikiMonteCarlo.append(monteCarlo(funkcja, dolnaGranica, gornaGranica, i))
+    bledyProstokaty = [(abs(a - wynik) / abs(wynik)) for a in wynikiProstokaty]
+    bledyTrapezy = [(abs(a - wynik) / abs(wynik))  for a in wynikiTrapezy]
+    bledyMonteCarlo = [(abs(a - wynik) / abs(wynik)) for a in wynikiMonteCarlo]
+    print(bledyProstokaty)
+    print(bledyTrapezy)
+    print(bledyMonteCarlo)
+    plt.title("Porównanie metod całkowania")
+    plt.xlabel('liczba przedziałów')
+    plt.ylabel('błąd wzgłedny')
+    x = range(a, b)
+    plt.plot(x, bledyTrapezy, label='metoda trapezów')
+    plt.plot(x, bledyProstokaty, label='metoda prostokatów')
+    # plt.plot(x, bledyMonteCarlo, label='monte carlo')
+    plt.legend()
+    plt.show()
+
+
+# print(prostokaty(funkcja, 0, 4, 10))
+# print(trapezy(funkcja, 0, 4, 10))
+# print(monteCarlo(funkcja, 0, 4, 10000))
+wykres()
