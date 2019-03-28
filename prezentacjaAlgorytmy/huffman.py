6bae93f1560998b1829635319c91a9291e74fc4e
hcnt <jacek.kusmierczyk@wp.pl>
2019-03-29 00:52:27 +0100

    huffman

--- prezentacjaAlgorytmy/huffman.py
+++ prezentacjaAlgorytmy/huffman.py
@@ -8,29 +8,27 @@ def get_2_najmniejsze_argumenty(slownik):
     slownikKopia.pop(min1)
     min2 = min(slownikKopia, key=slownikKopia.get)
     wartosc2 = slownikKopia.get(min2)
-    return (min1, min2), wartosc1 + wartosc2
+    return (min2, min1), wartosc1 + wartosc2
 
 
-# get_2_najmniejsze_argumenty(
+def dodajDoWszystkichElementow(tablica, slownik, znak):
+    for x in tablica:
+        if type(x) == str:
+            slownik[x] += znak
+        else:
+            dodajDoWszystkichElementow(x, slownik, znak)
 
 
-def stworzSlownik(drzewo, slownik):
-    if type(drzewo[0]) == str:
-        slownik[drzewo[0]] += '0'
-    elif type(drzewo[0]) == tuple:
-        slownik[drzewo[0]] += '0'
-        stworzSlownik(drzewo[0], slownik)
-
-    if type(drzewo[1]) == str:
-        slownik[drzewo[1]] += '1'
-    elif type(drzewo[0]) == tuple:
-        slownik[drzewo[1]] += '1'
-        stworzSlownik(drzewo[1], slownik)
-
+def wypelnijSlownik(drzewo,slownik):
+    for i, x in enumerate(drzewo):
+        if type(x) == str:
+            slownik[x] += str(i)
+        elif type(x) == tuple:
+            dodajDoWszystkichElementow(x, slownik, str(i))
+            wypelnijSlownik(x, slownik)
 
 def utworz_drzewo(probka):
     ilosci_liter = Counter(probka)
-    # prawdopodobienstwa = map(lambda a: ilosci_liter.get(a) / len(text), ilosci_liter)
     prawdopodobienstwa = {}
     for a in ilosci_liter:
         prawdopodobienstwa[a] = ilosci_liter.get(a) / len(probka)
@@ -40,12 +38,29 @@ def utworz_drzewo(probka):
         prawdopodobienstwa.pop(wezel[0])
         prawdopodobienstwa.pop(wezel[1])
         prawdopodobienstwa[wezel] = suma_prawdop
-    return list(prawdopodobienstwa.keys())[0]
+    litery = list(ilosci_liter.keys())
+    slownik = {x: '' for x in litery}
+    drzewo = list(prawdopodobienstwa.keys())[0]
+    wypelnijSlownik(drzewo, slownik)
+    return drzewo, slownik
 
 
-drzewo = utworz_drzewo('PIEEES')
-slownik = {}
-slownik.setdefault('')
+def koduj_H(napis, slownik):
+    return ''.join([slownik[x] for x in napis])
 
-print(stworzSlownik(drzewo, slownik))
-print(slownik)
+
+def dekoduj_H(napis, drzewo):
+    napis_zdekodowany = ''
+    pozycja_w_drzewie = drzewo
+    for x in napis:
+        pozycja_w_drzewie = pozycja_w_drzewie[int(x)]
+        if type(pozycja_w_drzewie) == str:
+            napis_zdekodowany += pozycja_w_drzewie
+            pozycja_w_drzewie = drzewo
+    return napis_zdekodowany
+
+
+drzewo, slownikLiter = utworz_drzewo('PIEEES')
+zakodowany_pies = koduj_H('PIEEES', slownikLiter)
+print(zakodowany_pies)
+print(dekoduj_H(zakodowany_pies, drzewo))
